5837a6103f669704904cd6d50e6f51ab90b2b6d2
Phani Kumar Uppalapati <phaniu@codeaurora.org>
2015-07-01 09:37:00 +0200

    gemini: Add AANC tuning configuration file
    AANC (Adaptive Active Noise Cancellation) tuning
    configuration file contains mixer controls to setup
    the ANC loopback path for handset device. It is used
    by ACDB to collect the waveform for estimating handset
    device audio characteristics. Add tuning configuration
    for wcd9335 codec.
    
    Change-Id: I1e0092b0bfb25349a6c018c8c28a5fda44b03b7c

--- device.mk
+++ device.mk
@@ -88,6 +88,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml
 
