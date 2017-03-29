31722dc19dfbef7c1024bec7d5707a524560ba65
offain <fabian.leutenegger@bluewin.ch>
2017-03-29 22:37:24 +0200

    gemini: nuke cmhw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,9 +115,6 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # CM Hardware
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    $(DEVICE_PATH)/cmhw
 BOARD_USES_CYANOGEN_HARDWARE := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
