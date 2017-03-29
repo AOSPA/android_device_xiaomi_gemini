7a192f8b112f65fdcf966ecc79495d6e337f4406
offain <fabian.leutenegger@bluewin.ch>
2017-03-29 22:56:19 +0200

    gemini: build kernel with dtbs'

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,6 +58,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := lineageos_gemini_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8996
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_APPEND_DTB := true
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8996
