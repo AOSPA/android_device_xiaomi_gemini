d2506dd18f92832f50b2ab3923bc04f187f5c39e
Offain <fabian.leutenegger@bluewin.ch>
2017-10-30 17:39:27 +0100

    gemini: Don't build Snap
     * We build ParanoidCamera by default
    
    Change-Id: I64d3b51384926e704d791dcd7c506a8be8e942d1

--- device.mk
+++ device.mk
@@ -138,8 +138,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8996 \
-    libmm-qcamera \
-    Snap
+    libmm-qcamera
 
 # CNE
 PRODUCT_PACKAGES += \
