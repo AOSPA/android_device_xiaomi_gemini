ea4cf4038cc2dcebdc2859605b3e722722c384ea
Offain <fabian.leutenegger@bluewin.ch>
2018-02-07 15:46:52 +0100

    gemini: Rename lights HAL
     * To not interfere with other Xiaomi light HALs
    
    Change-Id: I5d44c94d3bfa6b3bb4b2370765d300a4fa5dd660

--- device.mk
+++ device.mk
@@ -279,7 +279,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_8996
+    android.hardware.light@2.0-service.gemini
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
