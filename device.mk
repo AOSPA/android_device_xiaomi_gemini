5cb4a3d027e2ff9861f5860a3c3675fa83e53ebb
Offain <fabian.leutenegger@bluewin.ch>
2018-02-07 16:05:54 +0100

    gemini: Rename IR HAL
     * To not interfere with other Xiaomi ir HALs
    
    Change-Id: I149fae132f6bfed88dc35621dafcecc9165060b3

--- device.mk
+++ device.mk
@@ -165,7 +165,7 @@ PRODUCT_PACKAGES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi_8996
+    android.hardware.ir@1.0-service.gemini
 
 # Device init scripts
 PRODUCT_PACKAGES += \
