5121f9a65866d45b6868c1baf50a075bae623566
Offain <fabian.leutenegger@bluewin.ch>
2018-03-01 22:15:39 +0100

    gemini: Remove duplicated PRODUCT_BOOT_JARS
     * It already defined in our platform
    
    Change-Id: I62eca1083dec75c8f390e6e27cfb50bb59b81e25

--- device.mk
+++ device.mk
@@ -372,9 +372,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
